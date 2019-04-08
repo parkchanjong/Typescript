63449a07a81b420677225fb04f56c6bef40ba5de
parkchanjong <7000cj@gmail.com>
2019-04-08 21:40:48 +0900

    val

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('hi');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Chanjong", age = 29, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
