1de386260c0f2f31cd7db6d426b136cdf264563f
parkchanjong <7000cj@gmail.com>
2019-04-25 16:23:15 +0900

    ts-class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-interface Human {
-	name: string;
-	age: number;
-	gender: string;
+class Human {
+	public name: string;
+	public age: number;
+	public gender: string;
+	constructor(name: string, age: number, gender: string) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
 }
 
-const person = {
-	name: 'cj',
-	age: 27,
-	gender: 'male',
-};
+const cj = new Human('cj', 23, 'male');
 
 const sayHi = (person: Human): string => {
 	return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(cj));
 
 export {};
