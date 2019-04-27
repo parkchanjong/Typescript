53dd5708fb452b97fdd975ad507f1db85fa67344
parkchanjong <7000cj@gmail.com>
2019-04-28 00:39:21 +0900

    C-block

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-class Human {
-	public name: string;
-	public age: number;
-	public gender: string;
-	constructor(name: string, age: number, gender: string) {
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
+import * as CryptoJS from 'crypto-js';
+
+class Block {
+	static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+		CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+	public index: number;
+	public hash: string;
+	public previousHash: string;
+	public data: string;
+	public timestamp: number;
+
+	constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+		this.index = index;
+		this.hash = hash;
+		this.previousHash = previousHash;
+		this.data = data;
+		this.timestamp = timestamp;
 	}
 }
 
-const cj = new Human('cj', 23, 'male');
+const genesisBlock: Block = new Block(0, '02020020202', '', 'Hello', 123456);
 
-const sayHi = (person: Human): string => {
-	return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-console.log(sayHi(cj));
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
